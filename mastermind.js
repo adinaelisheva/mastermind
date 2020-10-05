1bf1c993a559506871cf731c869d0a61ca5c7402
Adina <adina.elisheva@gmail.com>
2020-10-05 00:11:05 -0400

    Adding support for clicking gems, and disabling buttons
    And some general cleanup
    
    Fixes #2

--- mastermind.js
+++ mastermind.js
@@ -4,8 +4,10 @@ let currScore;
 let code = [];
 let colors = ['one','two','three','four','five','six'];
 let playing = true;
+let resetButton;
+let submitButton;
 
-const setup = () => {
+function setup(){
   const pbuttons = document.querySelectorAll('.piece.clickable');
   pbuttons.forEach((pb) => {
     pb.addEventListener('click', () => {
@@ -16,11 +18,13 @@ const setup = () => {
   document.querySelector('.giveup').addEventListener('click',() => {
     giveUp();
   });
-  document.querySelector('.reset').addEventListener('click',() => {
+  resetButton = document.querySelector('.reset');
+  resetButton.addEventListener('click',() => {
     if (!playing) { return; }
     resetCurrentRow();
   });
-  document.querySelector('.submit').addEventListener('click',() => {
+  submitButton = document.querySelector('.submit');
+  submitButton.addEventListener('click',() => {
     if (!playing) { return; }
     submitCurrentRow();
   });
@@ -34,42 +38,74 @@ const setup = () => {
 
 };
 
-const handlePieceClick = (button) => {
+function handlePieceClick(button){
   const color = button.classList[button.classList.length - 1];
   addPieceByColor(color);
 }
 
-const addPieceByColor = (color) => {
-  const pieces = currRow.querySelectorAll('.piece');
-  pieces[currInd].className = `piece ${color}`;
-  pieces[currInd].setAttribute('color', color);
-  currInd = (currInd + 1) % 4;
-  pieces[currInd].classList.add('current');
-};
-
-const resetCurrentRow = () => {
-  const pieces = currRow.querySelectorAll('.piece');
-  pieces.forEach((p) => {
-    p.className = 'piece';
-  });
-  currInd = 0;
-  pieces[0].classList.add('current');
-};
-
-const isCurrentRowComplete = () => {
-  let isComplete = true;
-  currRow.querySelectorAll('.piece').forEach((p) => {
-    const color = p.className.replace('current','').replace('piece','').replace(' ','');
-    if(!color.length) {
-      // this piece doesn't have a color class
-      isComplete = false;
-    }
-  });
-  return isComplete;
+function updateButtonDisabledState() {
+  const numFilled = countCurrRowFilled();
+  if (numFilled === 0) {
+    resetButton.classList.add('disabled');
+  } else {
+    resetButton.classList.remove('disabled');
+  }
+  if (numFilled < 4) {
+    submitButton.classList.add('disabled');
+  } else {
+    submitButton.classList.remove('disabled');
+  }
 }
 
-const submitCurrentRow = () => {
-  if (!isCurrentRowComplete()) {
+function addPieceByColor(color){
+  const pieces = currRow.querySelectorAll('.piece');
+  clearPieceColor(pieces[currInd]);
+  pieces[currInd].classList.add(color);
+  pieces[currInd].setAttribute('color', color);
+  selectIndex(currInd + 1);
+  updateButtonDisabledState();
+};
+
+function selectIndex(index) {
+  const pieces = currRow.querySelectorAll('.piece');
+  pieces[currInd].classList.remove('current');
+  currInd = index % 4;
+  pieces[currInd].classList.add('current');
+}
+
+function clearPieceColor(p) {
+  colors.forEach((c) => {
+    p.classList.remove(c);
+  });
+}
+
+function resetCurrentRow(){
+  const pieces = currRow.querySelectorAll('.piece');
+  pieces.forEach((p) => {
+    clearPieceColor(p);
+  });
+  selectIndex(0);
+  updateButtonDisabledState();
+};
+
+function countCurrRowFilled(){
+  let curRowFilled = 0;
+  currRow.querySelectorAll('.piece').forEach((p) => {
+    const color = p.className
+        .replace('current','')
+        .replace('clickable','')
+        .replace('piece','')
+        .trim();
+    if(color.length) {
+      // this piece has a color class
+      curRowFilled++;
+    }
+  });
+  return curRowFilled;
+}
+
+function submitCurrentRow(){
+  if (countCurrRowFilled() < 4) {
     alert('Current row incomplete!');
     return;
   }
@@ -80,11 +116,17 @@ const submitCurrentRow = () => {
   const guess = [];
   pieces.forEach((p) => {
     guess.push(p.getAttribute('color'));
-  })
+  });
   scoreGuess(guess);
 }
 
-const addNewRow = () => {
+function addNewRow(){
+  if (currRow) {
+    currRow.querySelectorAll('.piece.clickable').forEach((p) => {
+      p.classList.remove('clickable');
+      p.classList.add('unclickable');
+    });
+  }
   const board = document.querySelector('#board');
   const container = document.createElement('div');
   container.className = 'rowholder';
@@ -92,10 +134,13 @@ const addNewRow = () => {
   row.className = 'row';
   for(let i = 0; i < 4; i++) {
     let piece = document.createElement('div');
-    piece.className = 'piece';
+    piece.className = 'piece clickable';
     if(i === 0) {
       piece.classList.add('current');
     }
+    piece.addEventListener('click', () => {
+      selectIndex(i);
+    });
     row.appendChild(piece);
   }
 
@@ -111,9 +156,10 @@ const addNewRow = () => {
   currScore = score;
 
   board.scrollTop = board.scrollHeight;
+  updateButtonDisabledState();
 };
 
-const giveUp = () => {
+function giveUp(){
   addNewRow();
   for(let i = 0; i < 4; i++) {
     addPieceByColor(code[i]);
@@ -122,7 +168,7 @@ const giveUp = () => {
   endGame();
 }
 
-const win = () => {
+function win(){
   document.title = "Online Mastermind - You Win!"
   alert(`Correct! You Win!\nScore: ${document.querySelector('#board').childElementCount}`);
   const star = document.createElement('div');
@@ -132,14 +178,15 @@ const win = () => {
   endGame();
 }
 
-const endGame = () => {
+function endGame(){
   playing = false;
   document.querySelectorAll('.clickable').forEach((b) => {
     b.classList.remove('clickable');
+    b.classList.add('unclickable');
   });
 }
 
-const scoreGuess = (guess) => {
+function scoreGuess(guess){
   if (guess.join('') === code.join('')) {
     win();
     return;
